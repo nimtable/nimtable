e1b0f2025cc9df0f46e4b4e0d079819ca9002066
neverchanje <wutao.as.neverchanje@gmail.com>
2025-04-07 14:36:53 +0800

    rm types/data.ts

--- src/types/data.ts
+++
@@ -1,7 +0,0 @@
-// Types for the sidebar data structure
-export interface NamespaceTables {
-    name: string // full namespace name
-    shortName: string // last part of the namespace name
-    tables: string[]
-    children: NamespaceTables[]
-}
