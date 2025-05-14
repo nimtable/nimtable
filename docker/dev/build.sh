5f6355e237eac0b83c3261e65b3a8f487af6f7f0
xxchan <xxchan22f@gmail.com>
2025-05-14 16:01:44 +0800

    update docker
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- docker/dev/build.sh
+++ docker/dev/build.sh
@@ -10,3 +10,11 @@ cd "$SCRIPT_DIR"/../../backend || exit 1
 # Copy the jar to the dev directory, since it's in .dockerignore
 cp ./build/libs/nimtable-all.jar "$SCRIPT_DIR"/nimtable-all.jar
 
+# Build the frontend
+cd "$SCRIPT_DIR"/../.. || exit 1
+npm run build
+
+# Copy the build directory to the dev directory
+cp -r .next "$SCRIPT_DIR"/.next
+cp package.json "$SCRIPT_DIR"/package.json
+cp package-lock.json "$SCRIPT_DIR"/package-lock.json
