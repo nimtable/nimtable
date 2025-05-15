f8cea059a7823326e43cf68e944e26372d9fe707
xxchan <xxchan22f@gmail.com>
2025-05-15 23:05:56 +0800

    chore: bump eslint
    Can't get eslint run locally previously.
    
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,10 +8,12 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
+    files: ['**/*.{ts,tsx}'],
     plugins: {
       'unused-imports': unusedImports,
     },
     rules: {
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
@@ -21,8 +23,12 @@ export default tseslint.config(
           "varsIgnorePattern": "^_",
           "args": "after-used",
           "argsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_",
         },
       ]
     },
+  },
+  {
+    ignores: ['**/.next/**', '**/out/**', '**/dist/**', "*.config.*"],
   },
 );
