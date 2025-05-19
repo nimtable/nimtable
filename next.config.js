622847876fde8b9360094c18edabaa61a9e0e156
Tianchun Luo <41638002+Richardwinston001@users.noreply.github.com>
2025-05-19 15:23:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   // rewrites is only for development (npm run dev); it won't affect static export
   rewrites: () => [
     {
